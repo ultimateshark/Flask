1da9d04e9d0bd4727155717a8eae1c5b39ffc52a
root <manishpandey8858@gmail.com>
2018-08-15 01:49:34 +0530

    Flask

---
+++ jinja/app.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template,url_for,redirect
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello"
+
+@app.route("/firstpage")
+def firstpage():
+    return "My First Web Page!!!"
+
+
+@app.route("/secondpage/<int:num>")
+def secondpage(num):
+    if num==0:
+        return redirect(url_for("index"))
+    return str(num)
+
+
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug='True')
---
+++ login/app.py
@@ -0,0 +1,36 @@
+from flask import Flask,render_template,url_for,redirect,request
+from mysqldb import connection
+from passlib.hash import sha256_crypt
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    name=request.form['user']
+    passwd=request.form['passwd']
+    c,conn=connection()
+    c.execute("use example")
+    udata=c.execute("select * from user where name='{0}'".format(name))
+    udata=c.fetchone()
+    if sha256_crypt.verify(passwd,udata[3]):
+        return str(udata)
+    else:
+        return "wrong credential"
+
+@app.route("/signup",methods=["POST"])
+def signup():
+    c,conn=connection()
+    c.execute("use example")
+    name=request.form['user']
+    email=request.form['email']
+    passwd=sha256_crypt.encrypt(request.form['passwd'])
+    c.execute("INSERT INTO user (name,email,passwd) VALUES ('{0}','{1}','{2}')".format(name,email,passwd))
+    conn.commit()
+    c.close()
+    conn.close()
+    return "submitted"
+if __name__ == '__main__':
+    app.run(debug='True')
