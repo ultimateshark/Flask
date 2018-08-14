e7eb99e47de94cb44b570922e4c6f812940b5285
root <manishpandey8858@gmail.com>
2018-08-15 00:07:24 +0530

    basics

--- FlaskBasics/app.py
+++ FlaskBasics/app.py
@@ -1,5 +1,4 @@
 from flask import Flask,render_template,url_for,redirect
-
 app=Flask(__name__)
 
 @app.route("/")
@@ -10,13 +9,19 @@ def home():
 def firstpage():
     return "My First Web Page!!!"
 
+
 @app.route("/secondpage/<int:num>")
 def secondpage(num):
     if num==0:
         return redirect(url_for("index"))
     return str(num)
+
+
+
 @app.route("/index")
 def index():
     return render_template("index.html")
+
+
 if __name__ == '__main__':
     app.run(debug='True')
